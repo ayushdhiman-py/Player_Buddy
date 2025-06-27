ebac189ca9cbc9418f8171e08201bee9e9f1bb4c
ayushdhiman-py <ayushdhiman0107@gmail.com>
2025-06-27 12:16:38 +0530

    read more open

--- components/Home/PostItem.js
+++ components/Home/PostItem.js
@@ -1,55 +1,43 @@
 import React from "react";
 import { HiOutlineLocationMarker, HiOutlineCalendar } from "react-icons/hi";
 import UserInfo from "./UserInfo";
+
 const PLACEHOLDER = "./Images/placeholder.jpg";
-function PostItem({ post, modal = false }) {
+
+function PostItem({ post, modal = false, onClick = () => {} }) {
   return (
     <>
       {post ? (
         <div
-          className="max-w-sm bg-white border
- border-gray-200 rounded-lg shadow cursor-pointer
-  dark:bg-gray-800 dark:border-gray-700 "
+          className="max-w-sm bg-white border border-gray-200 rounded-lg shadow cursor-pointer dark:bg-gray-800 dark:border-gray-700"
         >
           <img
-            className="rounded-t-lg 
-        w-full h-[180px] object-cover"
+            className="rounded-t-lg w-full h-[180px] object-cover"
             src={post.image ? post.image : PLACEHOLDER}
             alt="banner"
           />
 
           <div className="p-5">
-            <h5
-              className="mb-2 text-xl 
-    font-bold tracking-tight 
-    text-gray-900 dark:text-white"
-            >
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
               {post.title}
             </h5>
             <div className="flex items-center text-orange-500 gap-2 mb-2">
               <HiOutlineCalendar className="text-[20px]" />
               {post.date}
             </div>
-            <div
-              className="flex items-center 
-      text-blue-500 gap-2 mb-2"
-            >
+            <div className="flex items-center text-blue-500 gap-2 mb-2">
               <HiOutlineLocationMarker className="text-[20px]" />
               {post.location}
             </div>
-            <p
-              className="mb-3 font-normal
-         text-gray-700 
-         dark:text-gray-400"
-            >
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
               {post.desc}
             </p>
 
             {!modal ? <UserInfo user={post} /> : null}
             {modal ? (
               <div>
-                <a
-                  href="#"
+                <button
+                  onClick={() => onClick(post)}
                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Read more
@@ -58,11 +46,10 @@ function PostItem({ post, modal = false }) {
                     className="w-4 h-4 ml-2 -mr-1"
                     fill="currentColor"
                     viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
                   </svg>
-                </a>
+                </button>
               </div>
             ) : null}
           </div>
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -12,13 +12,15 @@ import {
 } from "firebase/firestore";
 import PostItem from "../../components/Home/PostItem";
 import Toast from "../../components/Toast";
+import PostModal from "../../components/Home/PostModal";
 
 function Profile() {
   const { data: session } = useSession();
   const [userPost, setUserPost] = useState([]);
   const db = getFirestore(app);
   const [showToast, setShowToast] = useState(false);
-  
+  const [selectedPost, setSelectedPost] = useState(null);
+
   useEffect(() => {
     getUserPost();
   }, [session, showToast]);
@@ -44,35 +46,37 @@ function Profile() {
     setShowToast(true);
     window.location.reload();
   };
+
   return (
     <div className="p-6 mt-8">
-      {showToast ? (
+      {showToast && (
         <div className="absolute top-10 right-10">
           <Toast
             msg={"Post Deleted Successfully"}
             closeToast={() => setShowToast(false)}
           />
         </div>
-      ) : null}
-      <h2
-        className="text-[35px] 
-      font-extrabold text-blue-500"
-      >
-        Profile
-      </h2>
+      )}
+
+      <PostModal post={selectedPost} />
+
+      <h2 className="text-[35px] font-extrabold text-blue-500">Profile</h2>
       <p>Manage Your Post</p>
-      <div
-        className="grid grid-cols-1 
-    sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-    gap-5 mt-5 px-10"
-      >
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5 px-10">
         {userPost &&
-          userPost?.map((item, index) => (
+          userPost.map((item, index) => (
             <div key={index}>
-              <PostItem post={item} modal={true} />
+              <PostItem
+                post={item}
+                modal={true}
+                onClick={(post) => {
+                  setSelectedPost(post);
+                  window.my_modal_1.showModal();
+                }}
+              />
               <button
-                className="bg-red-400 w-full p-1 mt-1
-        rounded-md text-white"
+                className="bg-red-400 w-full p-1 mt-1 rounded-md text-white"
                 onClick={() => onDeletePost(item.id)}
               >
                 Delete
